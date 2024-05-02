f63e3e71d4278c90ff6f61b29b42233ae5a6aeaa
Albert Byiringiro <byiringiroalbert20@gmail.com>
2024-05-02 21:07:58 +0200

    Exercise: Matches 2 objects

--- map-set/iterableKeys.js
+++ map-set/iterableKeys.js
@@ -3,7 +3,9 @@ let map = new Map();
 map.set("name", "john");
 map.set("ribakare", "john");
 
-let arrKeys = Array.from(map.keys());
+// let arrKeys = Array.from(map.keys());
+// using spread operators
+let arrKeys = [...map.keys()];
 
 arrKeys.push("more");
 
