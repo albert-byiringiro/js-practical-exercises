16a1f0d81ebcac08cf10eca9c18fed76a7561186
Albert Byiringiro <byiringiroalbert20@gmail.com>
2024-05-03 11:11:29 +0200

    Exercise: Matches 2 objects

--- es6/objectMatches.js
+++ es6/objectMatches.js
@@ -2,11 +2,11 @@
 
 
 // Define a function called `matches` that takes two objects as arguments and checks if the first object contains all the key-value pairs of the second object.
-const objectMathes = (obj1, obj2) => {
-    // get the keys of second object
+const objectMatches = (obj1, obj2) => {
+    // check if the keys and values of each object match
     return Object.keys(obj1).every(key => obj2.hasOwnProperty(key) && obj1[key] === obj2[key]);
 }
 
-console.log(objectMathes({ age: 25, hair: 'long', beard: true }, { hair: 'long', beard: true })); // true
-console.log(objectMathes({ hair: 'long', beard: true }, { age: 25, hair: 'long', beard: true })); // false
-console.log(objectMathes({ hair: 'long', beard: true }, { age: 26, hair: 'long', beard: true })); // false
+console.log(objectMatches({ age: 25, hair: 'long', beard: true }, { hair: 'long', beard: true })); // true
+console.log(objectMatches({ hair: 'long', beard: true }, { age: 25, hair: 'long', beard: true })); // false
+console.log(objectMatches({ hair: 'long', beard: true }, { age: 26, hair: 'long', beard: true })); // false
