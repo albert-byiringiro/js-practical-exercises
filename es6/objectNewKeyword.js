bc556ec440d7fb0698f57df8e9b958c709e0b7e4
Albert Byiringiro <byiringiroalbert20@gmail.com>
2024-05-07 20:58:12 +0200

    Feat: Create an object using the NEW keyword - chal3

--- es6/objectNewKeyword.js
+++ es6/objectNewKeyword.js
@@ -42,3 +42,12 @@ console.log(mike.name); // -> Logs 'Mike'
 console.log(mike.age); //-> Logs 30
 // mike.greet(); //-> Logs 'hello'
 
+
+/*** CHALLENGE 7 ***/
+// add code here
+
+PersonConstructor.prototype.introduce = function () {
+    console.log(`Hi, my name is ${this.name}`);
+}
+
+mike.introduce(); // -> Logs 'Hi, my name is Mike'
