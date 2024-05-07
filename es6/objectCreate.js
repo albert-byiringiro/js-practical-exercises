e59c63d7cfc678c6564fba7d1e7866ec2d689000
Albert Byiringiro <byiringiroalbert20@gmail.com>
2024-05-07 20:51:56 +0200

    Feat: Create an object using object.create desc

--- es6/objectCreate.js
+++ es6/objectCreate.js
@@ -1,5 +1,11 @@
 /****************************************************************
-                       USING OBJECT.CREATE
+                       Using Object.create
+Challenge 2
+Inside personStore object, create a property greet where the value is a function that logs "hello".
+Challenge 3
+Create a function personFromPersonStore that takes as input a name and an age. When called, the function will create person objects using the Object.create method on the personStore object.
+Challenge 4
+Without editing the code you've already written, add an introduce method to the personStore object that logs "Hi, my name is [name]".
 ****************************************************************/
 
 /*** CHALLENGE 2 ***/
