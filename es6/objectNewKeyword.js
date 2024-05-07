6045e04c9f01ce7d5797701e14344f9652063314
Albert Byiringiro <byiringiroalbert20@gmail.com>
2024-05-07 20:56:33 +0200

    Feat: Create an object using the NEW keyword - desc

---
+++ es6/objectNewKeyword.js
@@ -0,0 +1,23 @@
+/****************************************************************
+                    Using the NEW keyword
+Challenge 5
+Create a function PersonConstructor that uses the this keyword to save a single property onto its scope called greet. greet should be a function that logs the string 'hello'.
+Challenge 6
+Create a function personFromConstructor that takes as input a name and an age. When called, the function will create person objects using the new keyword instead of the Object.create method.
+Challenge 7
+Without editing the code you've already written, add an introduce method to the PersonConstructor function that logs "Hi, my name is [name]".
+****************************************************************/
+
+/*** CHALLENGE 5 ***/
+
+function PersonConstructor() {
+    // add code here
+    this.greet = () => console.log("hello");
+
+}
+
+
+// /********* Uncomment this line to test your work! *********/
+const simon = new PersonConstructor;
+simon.greet(); // -> Logs 'hello'
+
