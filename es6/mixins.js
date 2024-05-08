93b0585d467475cde52d046201b4292cf01e6ce5
Albert Byiringiro <byiringiroalbert20@gmail.com>
2024-05-08 21:01:18 +0200

    core: Mixin exercise description

---
+++ es6/mixins.js
@@ -0,0 +1,4 @@
+/****************************************************************
+EXTENSION: Mixins
+Mixins are a tool in object-oriented programming that allows objects to be given methods and properties beyond those provided by their inheritance. For this challenge, complete the missing code, giving all of the robotMixin properties to robotFido. Do this in a single line of code, without adding the properties individually.
+****************************************************************/
