853cb65dcf5476d1c218eae07bca6f3c02e4d4e7
Albert Byiringiro <byiringiroalbert20@gmail.com>
2024-05-08 21:02:13 +0200

    Feat: Challenge Setup

--- es6/mixins.js
+++ es6/mixins.js
@@ -2,3 +2,19 @@
 EXTENSION: Mixins
 Mixins are a tool in object-oriented programming that allows objects to be given methods and properties beyond those provided by their inheritance. For this challenge, complete the missing code, giving all of the robotMixin properties to robotFido. Do this in a single line of code, without adding the properties individually.
 ****************************************************************/
+
+class Dog {
+    constructor() {
+        this.legs = 4;
+    }
+    speak() {
+        console.log('Woof!');
+    }
+}
+
+const robotMixin = {
+    skin: 'metal',
+    speak: function () { console.log(`I have ${this.legs} legs and am made of ${this.skin}`) },
+}
+
+let robotFido = new Dog();
