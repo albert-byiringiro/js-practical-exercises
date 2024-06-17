609341f7627fb4f1b30f6f54d0cdad23a2b08034
Albert Byiringiro <byiringiroalbert20@gmail.com>
2024-06-17 07:44:18 +0200

    feat: fetch concurently multiple APIs - updated

--- async/exercises/fetchMultipleApis.js
+++ async/exercises/fetchMultipleApis.js
@@ -1,3 +1,33 @@
+function downloadContents(urls) {
+const promises = urls.map(url => {
+  return new Promise((resolve, reject)=> {
+    fetch(url).then(response => {
+      if(!response.ok){
+        throw new Error("Oops! We are not online")
+      } else {
+        return response.json();
+      }
+    }).then(data => resolve(data)).catch(error => reject(error))
+  })
+});
+  
+  return Promise.all(promises)
+}
+
+// Usage example:
+const urls = [
+  'https://jsonplaceholder.typicode.com/posts/1',
+  'https://jsonplaceholder.typicode.com/posts/2',
+  'https://jsonplaceholder.typicode.com/posts/3'
+];
+
+downloadContents(urls)
+  .then(contents => {
+    console.log('Downloaded contents:', contents);
+  })
+  .catch(error => {
+    console.log('Error:', error.message);
+  });
 const apiUrls = [
     'https://jsonplaceholder.typicode.com/posts/4',
     'https://jsonplaceholder.typicode.com/posts/5',
