97efbb6b3350248b2abcd142416d1ebf9fb4f008
Albert Byiringiro <byiringiroalbert20@gmail.com>
2024-06-20 23:19:52 +0200

    exercise: print tick every other second

---
+++ async/exercises/beginner/tickInterval.js
@@ -0,0 +1,13 @@
+/*
+3. Using `setInterval`, write a function that logs "Tick" to the console every second.
+
+
+function everyTick(){
+  setInterval(()=>{
+    console.log("Tick")
+  }, 1000)
+}
+
+everyTick()
+
+*/
