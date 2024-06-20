86473bb899f15025dd3ca5846f4a1812c5067867
Albert Byiringiro <byiringiroalbert20@gmail.com>
2024-06-20 23:18:04 +0200

    exercise: add callback timer

---
+++ async/exercises/beginner/callbackTimer.js
@@ -0,0 +1,14 @@
+/* 2. Create a function that takes a callback and executes it after 3 seconds using `setTimeout`.
+
+
+function takeCall(callback){
+  setTimeout(callback, 3000);
+}
+
+function remember(){
+  console.log("Don't forget...");
+}
+
+takeCall(remember);
+
+*/
