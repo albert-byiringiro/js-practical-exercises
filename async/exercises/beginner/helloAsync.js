48956455ffafc52b716e9a092a61f04bea35d271
Albert Byiringiro <byiringiroalbert20@gmail.com>
2024-06-20 23:16:20 +0200

    exercise: add hello async

---
+++ async/exercises/beginner/helloAsync.js
@@ -0,0 +1,10 @@
+/* 1. Write a function that uses `setTimeout` to log "Hello, World!" to the console after 2 seconds.
+*/
+
+function helloWord() {
+  setTimeout(() => {
+    console.log("Hello, world!");
+  }, 2000)
+}
+
+helloWord()
