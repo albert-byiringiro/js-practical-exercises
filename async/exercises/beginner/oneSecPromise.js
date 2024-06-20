d73653365615b88c5469eaa6162aaedc3d544c4a
Albert Byiringiro <byiringiroalbert20@gmail.com>
2024-06-20 23:21:55 +0200

    exercise: resolve after 1 sec

---
+++ async/exercises/beginner/oneSecPromise.js
@@ -0,0 +1,13 @@
+
+/* 4. Write a Promise that resolves with "Success!" after 1 second. Log the result to the console.
+
+const oneSec = new Promise((resolve, reject)=>{
+  setTimeout(()=>{
+    resolve("Success!");
+  }, 1000)
+})
+
+oneSec
+  .then((response)=> console.log(response))
+  
+*/
