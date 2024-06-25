7cc51ce66ac60690bdb11d948ce994ec85ac9924
Albert Byiringiro <byiringiroalbert20@gmail.com>
2024-06-25 15:44:02 +0200

    post request http using a xhr in a promise

--- async/exercises/beginner/xhrMagic.js
+++ async/exercises/beginner/xhrMagic.js
@@ -26,7 +26,8 @@ xhrReq()
 
 /*
 Create a function called myFetch that should work as a simple version of the native fetch() API. The function myFetch should use the XMLHttpRequest to make a GET Request and return a promise that resolves with the request’s response and rejects with an error if any.
-*/
+
+
 
 function myFetch(url) {
   return new Promise((resolve, reject) => {
@@ -49,5 +50,45 @@ function myFetch(url) {
 }
 
 myFetch('https://jsonplaceholder.typicode.com/todos/1')
+  .then(data => console.log(data))
+  .catch(error => console.error(error))
+*/
+
+// POST request
+
+function myFetch(url) {
+  return new Promise((resolve, reject) => {
+    const xhr = new XMLHttpRequest();
+    xhr.open('POST', url, true);
+    xhr.setRequestHeader("Content-Type", 'application/json');
+
+    // the data
+    const datum = {
+      "userId": 1,
+      "id": 101,
+      "title": "God's been so good...",
+      "completed": true
+    };
+
+    // serialize the data
+    const jsonData = JSON.stringify(datum);
+
+    xhr.onload = () => {
+      if (xhr.status >= 200) {
+        resolve(xhr.responseText)
+      } else {
+        reject(`HTTP Error: ${xhr.status} - ${xhr.statusText}`);
+      }
+    }
+
+    xhr.onerror = () => {
+      reject(`Network error`)
+    }
+
+    xhr.send(jsonData);
+  });
+}
+
+myFetch('https://jsonplaceholder.typicode.com/todos')
   .then(data => console.log(data))
   .catch(error => console.error(error))
