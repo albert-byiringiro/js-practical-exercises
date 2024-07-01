07834aab0465c633505440d38ac56e9198357370
Albert Byiringiro <byiringiroalbert20@gmail.com>
2024-07-01 12:45:24 +0200

    add app.js

---
+++ async/exercises/dom-async/the-weather-app/js/app.js
@@ -0,0 +1,12 @@
+const store = {
+    apiKey: "8a0fb5fc01c4876b3fd44716f18a4141",
+    locationName: 'kigali, rwanda',
+}
+
+let weatherApp;
+
+window.onload = async function () {
+    weatherApp = new Weather(store);
+    await weatherApp.init();
+    console.log(weatherApp.data.current);
+}
