8cca20bd6d2689fcae71e85db538f5502b1beafa
Albert Byiringiro <byiringiroalbert20@gmail.com>
2024-08-14 15:47:59 +0200

    add counterslice

--- gate/my-project/src/counterSlice.jsx
+++ gate/my-project/src/counterSlice.jsx
@@ -1,17 +1,15 @@
 import { createSlice } from "@reduxjs/toolkit";
 
-
 const counterSlice = createSlice({
     name: "counter",
     initialState: 0,
     reducers: {
         increment: (state) => state + 1,
         decrement: (state) => state - 1,
-        incrementByAmount: (state, action) => state + action.payload
-    },
+        incrementByNumber: (state, action) => state + action.payload,
+    }
 });
 
-
-export const { increment, decrement, incrementByAmount } = counterSlice.actions;
+export const { increment, decrement, incrementByNumber} = counterSlice.actions;
 
 export default counterSlice.reducer;
--- gate/my-project/src/store.js
+++
@@ -1,9 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import counterReducer from "./assets/useReducer/counterReducer";
-
-
-const store = configureStore({
-    reducer: counterReducer,
-})
-
-export default store;
